c99a58ccea8324f7b56faa3dda1222485a936fe0
thisissahin <thisissahin@gmail.com>
2022-10-28 14:19:18 +0400

    initial commit

--- app/src/main/java/az/sahin/playground/MainActivity.kt
+++ app/src/main/java/az/sahin/playground/MainActivity.kt
@@ -1,11 +1,19 @@
 package az.sahin.playground
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        AppCenter.start(
+            application, "1cf365d3-886c-4511-b1c1-cc041255b3f3",
+            Analytics::class.java, Crashes::class.java
+        )
     }
 }
